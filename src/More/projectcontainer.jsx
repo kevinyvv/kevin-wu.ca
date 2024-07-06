2a576bc01757eae9f3cf86632e2305416721d753
Kevin <kevin31422005@gmail.com>
2024-07-06 14:42:24 -0400

    quick ui fix for iphone

--- src/More/projectcontainer.jsx
+++ src/More/projectcontainer.jsx
@@ -15,10 +15,10 @@ const Projectcontainer = ( {title, imag, description, link, stack} ) => {
         </div>
         <div className='flex justify-between mt-2'>
             <img src={imag}  alt="Project Image" className="h-12 w-12 lg:w-20 lg:h-20 rounded-lg"/>
-            <p className='ml-4 h-[10svh] w-full text-start text-sm lg:text-md overflow-hidden'> 
+            <p className='ml-4 h-[10svh] w-full text-start text-sm lg:text-md overflow-auto'> 
             {description}  </p>
         </div>
-        <p className='relative mt-2 bottom-0 text-sm lg:text-md text-start'> 
+        <p className='relative mt-2 bottom-0 text-xs lg:text-sm text-start invisible sm:visible'> 
           {stack?.map((tech, index) => (
             <b key={index} className='mr-4 overflow-hidden bg-gray-300
              dark:bg-gray-400 rounded-xl p-1'>
